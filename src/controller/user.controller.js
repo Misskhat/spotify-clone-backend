eab196b68e8a453ffc7c1aeeb98c5995dc5d1544
Misskhat <misskhat.hossen@gmail.com>
2026-04-20 06:46:56 +0600

    user login controller setup

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,33 +1,70 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const cookieParser = require("cookie-parser");
-const userModel = require("../model/user.model");
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const cookieParser = require('cookie-parser')
+const userModel = require('../model/user.model')
 
 const registration = async (req, res) => {
-  const { userName, email, password, role = "user" } = req.body;
+  const { userName, email, password, role = 'user' } = req.body
   const isUserNameExit = await userModel.findOne({
     $or: [{ userName }, { email }],
-  });
+  })
   if (isUserNameExit) {
     return res.send({
-      message: "User already exist",
-    });
+      message: 'User already exist',
+    })
   }
-  const passwordHash = await bcrypt.hash(password, 10);
+  const passwordHash = await bcrypt.hash(password, 10)
   const newUser = await userModel.create({
     userName,
     email,
     password: passwordHash,
     role,
-  });
+  })
 
-  const token = jwt.sign({ _id: newUser._id }, process.env.JWT_TOKEN);
-  res.cookie("token", token);
+  const token = jwt.sign({ _id: newUser._id }, process.env.JWT_TOKEN)
+  res.cookie('token', token)
   res.status(200).json({
-    message: "User successfully register",
+    message: 'User successfully register',
     user: newUser,
-  });
-};
+  })
+}
 
-module.exports = { registration };
+const loginUser = async (req, res) => {
+  const { userName, email, password, role = 'user' } = req.body
+  const user = userModel.findOne({
+    $or: [{ userName }, { email }],
+  })
+
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid credential' })
+  }
+
+  const isPassword = await bcrypt.compare(password, user.password)
+
+  if (!isPassword) {
+    return res.status(401).json({ message: 'Invalid credential' })
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_TOKEN,
+  )
+
+  res.cookie('token', token)
+
+  res.status(200).json({
+    message: 'Login Successfully',
+    user: {
+      id: user._id,
+      userName: user.userName,
+      email: user.email,
+      role: user.role,
+    },
+  })
+}
+
+module.exports = { registration, loginUser }
